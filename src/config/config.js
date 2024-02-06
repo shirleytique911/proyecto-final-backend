9d894cd867a4dbd9c54110109b42ed0981f076b1
shirleytique <shirleytique911@gmail.com>
2024-02-06 11:45:13 -0500

    railway

--- src/config/config.js
+++ src/config/config.js
@@ -1,9 +1,11 @@
 import dotenv from 'dotenv';
 
 dotenv.config();
+console.log(process.env.DB_CONNECTION_STRING);  // Agrega esta línea para imprimir el valor
 
 export default {
     persistence: process.env.PERSISTENCE,
     mongo_url: process.env.DB_CONNECTION_STRING,
     port: process.env.PORT
 };
+
